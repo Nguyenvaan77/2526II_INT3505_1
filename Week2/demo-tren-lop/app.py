bed3baf8aa5b05b6310ad069f5af2d7e47ed4399
Nguyenvaan77 <anvan16022005@gmail.com>
2026-03-12 12:32:13 +0700

    lecture 2: version1: client-server

--- Week2/demo-tren-lop/app.py
+++ Week2/demo-tren-lop/app.py
@@ -1,30 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Dữ liệu giả lập
+# Dữ liệu nằm ở Server
 tasks = [
-    {
-        "id": 1,
-        "name": "Học REST"
-    }
+    {"id": 1, "name": "Học nguyên tắc Client-Server", "status": "Doing"},
+    {"id": 2, "name": "Viết báo cáo Ver 1", "status": "Pending"}
 ]
 
 # Sai lầm 1: Url chứa động từ (Vi phạm nguyên tắc uniform interface) 
 @app.route('/get-all-tasks-now', methods=['GET'])
 def get_tasks():
-    # Sai lầm 2: Trả về HTML trực tiếp (vi phạm Client-Server)
-    html = "<h1>Danh sách công việc</h1><ul>"
-    for t in tasks:
-        html += f"<li>{t['name']}</li>"
-    html += "</ul>"
-    return html
+    #Trả về JSON thay vì HTML
+    #Biến Object List thành JSON để trả về Client
+    return jsonify(tasks)
 
 # Sai lầm 3: Dùng GET để cập nhật thay vì PUT
 @app.route('/update-task-by-id?id=1', methods=['GET']) 
 def update_task():
     tasks[0]['name'] = "Đã học xong REST"
-    return "Xong rồi!"
+    return jsonify(tasks[0])
 
 if __name__ == '__main__':
     app.run(port=5000)
