9ebc3bb938a5ff698cf4ab79e52e01963576e1a9
Nguyenvaan77 <anvan16022005@gmail.com>
2026-04-03 04:30:29 +0700

    complete

--- Week5/task-manager-clone-from-week2/app.py
+++ Week5/task-manager-clone-from-week2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from routes.v1_routes import v1
 from routes.v2_routes import v2
@@ -10,6 +10,11 @@ app = Flask(__name__)
 app.register_blueprint(v1)
 app.register_blueprint(v2)
 app.register_blueprint(v3)
+
+
+@app.route("/")
+def management_dashboard():
+    return render_template("dashboard.html")
 
 
 if __name__ == "__main__":
